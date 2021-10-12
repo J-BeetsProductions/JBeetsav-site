4a202d9edff3a745c0f0cd5c95c8ab6b0daaabee
Joshua-Beets <86432668+Joshua-Beets@users.noreply.github.com>
2021-10-12 11:51:38 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-import "./vender/font-awesome/css/all.css";
-import "./assets/w3.css";
-import Main from "./components/Main";
-import Links from "./Links";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Home from "./components";
+import Resume from "./components/Resume";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
-        < Main />   
-        <Links />   
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
